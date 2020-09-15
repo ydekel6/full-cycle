2986d9b67991e64b7320fe9028772da38b94c4d9
Yuval Dekel <ydekel6@gmail.com>
2020-09-15 15:45:59 +0300

    cleaning code and fixing logging problems

--- aws-call.py
+++ aws-call.py
@@ -1,16 +1,15 @@
 import boto3
 import json
-import datetime
+from datetime import datetime
 import logging
-import pythonjsonlogger
-from pythonjsonlogger import jsonlogger
+logging.basicConfig(level=logging.INFO)  #allows seeing INFO level logs
 
 #starting a pipeline between aws to the 
 ec2 = boto3.client('ec2')
 
 #added the function the fix JSON problem with the datetime exportation of AWS
 def datetime_handler(x):
-    if isinstance(x, datetime.datetime):
+    if isinstance(x, datetime):
         return x.isoformat()
     raise TypeError("Unknown type")
 
@@ -27,49 +26,31 @@ names_list = []
 running_time_list = []
 num_of_instances = len(instances)
 count=0
-time = str(datetime.datetime.today())
-threadName = 'MainThread'
-message = 'testing K8S REPORTING'
-levelname = 'INFO'
-name = 'K8S REPORTS'
+
 
 while count < num_of_instances:
     instance = instances['Reservations'][count]['Instances'][0]
     publicip_list.append(instance['PublicIpAddress'])
-    running_time_list.append(str(datetime.datetime.now().replace(tzinfo=None)-instances['Reservations'][0]['Instances'][0]['LaunchTime'].replace(tzinfo=None)))
+    running_time_list.append(str(datetime.now().replace(tzinfo=None)-instances['Reservations'][0]['Instances'][0]['LaunchTime'].replace(tzinfo=None)))
     for j in instance["Tags"]:
         if j["Key"] == "Name":
             #get only the tag "Name"
             names_list.append(j["Value"])
     count=count+1
 
+#starting a logger
+logger = logging.getLogger('K8S REPORTS')
 
 report={
     'threadName':'MainThread',
-    "name": "K8S REPORTS",
-    "time": datetime.datetime.today(),
-    "running instances": num_of_instances,
-    "instance IPs": publicip_list,
+    'name': 'K8S REPORTS',
+    'time': datetime.now().strftime("%d/%m/%Y"),
+    'running instances': num_of_instances,
+    'instance IPs': publicip_list,
     'instance name': names_list,
     'running time': running_time_list,
-    'message': "testing K8S REPORTING",
-    "levelname": "INFO",
+    'message': 'testing K8S REPORTING',
+    'levelname': 'INFO'
 }
 
-class CustomJsonFormatter(jsonlogger.JsonFormatter):
-    def add_fields(self, log_record, record, message_dict):
-        return super().add_fields(log_record, record, message_dict)
-        
-# starting the JSON to the handler
-handler = logging.StreamHandler()
-# setting the format for the report
-#format_str = '%(threadName)%(name)%(time)%(num_of_instances)'
-format_str = '%(message)%(levelname)%(name)%(asctime)'
-formatter = pythonjsonlogger.jsonlogger.JsonFormatter(format_str)
-handler.setFormatter(formatter)
-logger = logging.getLogger()
-logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
-# Normally we would attach the handler to the root logger, and this would be unnecessary
-logger.debug(formatter)
-logger.propagate = False
+logger.info(report)
