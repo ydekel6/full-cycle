2c8218f8ce64fa3271822510b3fb3623aaf9026d
Yuval Dekel <ydekel6@gmail.com>
2020-09-23 14:41:35 +0300

    updated script and added multistage Dockerfile with tests

---
+++ Dockerfile
@@ -0,0 +1,51 @@
+# Phase I - Builder source
+FROM python:latest as builder
+# PYTHONUNBUFFERED Force logging to stdout / stderr not to be buffered into ram  
+ENV PYTHONNUNBUFFERED=1
+WORKDIR /usr/src/app
+COPY /flask-api ./
+WORKDIR /wheels
+COPY requirements.txt ./
+RUN pip wheel -r ./requirements.txt
+
+# Phase II - Linting the code
+FROM eeacms/pylint:latest as linting
+WORKDIR /code
+COPY --from=builder /usr/src/app/pylint.cfg /etc/pylint.cfg
+COPY --from=builder /usr/src/app/*.py ./
+COPY --from=builder /usr/src/app/api ./api
+RUN ["/docker-entrypoint.sh", "pylint"]
+
+# Phase III Running Sonarqube scanner (Sonarqube server also required)
+FROM newtmitch/sonar-scanner as sonarqube
+WORKDIR /usr/src
+COPY --from=builder /usr/src/app/*.py ./
+COPY --from=builder /usr/src/app/api ./api
+RUN sonar-scanner -Dsonar.projectBaseDir=/usr/src
+
+# Phase IV - Unit testing
+FROM python:latest as unit-tests
+WORKDIR /usr/src/app
+# Copy all packages instead of rerunning pip install
+COPY --from=builder /wheels /wheels
+RUN     pip install -r /wheels/requirements.txt \
+                      -f /wheels \
+       && rm -rf /wheels \
+       && rm -rf /root/.cache/pip/* 
+
+COPY --from=builder /usr/src/app/ ./
+RUN ["make", "test"]
+
+# Phase V - running the script itself
+FROM python:3.8-slim as serve
+WORKDIR /usr/src/app
+# Copy all packages instead of rerunning pip install
+COPY --from=builder /wheels /wheels
+RUN     pip install -r /wheels/requirements.txt \
+                      -f /wheels \
+       && rm -rf /wheels \
+       && rm -rf /root/.cache/pip/* 
+
+COPY --from=builder /usr/src/app/*.py ./
+COPY --from=builder /usr/src/app/api ./api
+CMD ["python", "aws-call.py"]
--- flask-api/aws-call.py
+++ flask-api/aws-call.py
@@ -1,42 +1,46 @@
-import boto3
+# written with python 3.8.5
 from datetime import datetime
 import logging
-logging.basicConfig(level=logging.INFO)  #allows seeing INFO level logs
+import boto3
+logging.basicConfig(level=logging.INFO)  # allows seeing INFO level logs
 
-#starting a pipeline between aws to the 
+# starting an ec2 instance
 ec2 = boto3.client('ec2')
 
 
 # create a list of instances according to the filters chosen.
 # this example filters all the ec2 instances that are running.
-instances = ec2.describe_instances(Filters=[{'Name' : 'instance-state-name','Values' : ['running']}])
+instances = ec2.describe_instances(
+    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 
-#testing JSON output
-#print(json.dumps(instances['Reservations'][0]['Instances'][0]['LaunchTime'], indent=4, sort_keys=True, default=datetime_handler))
+# testing JSON output
+# print(json.dumps(instances['Reservations'][0]['Instances'][0]['LaunchTime']
+# , indent=4, sort_keys=True, default=datetime_handler))
 
 
 publicip_list = []
 names_list = []
 running_time_list = []
 num_of_instances = len(instances['Reservations'][0]['Instances'])
-count=0
+count = 0
 
 
 while count < num_of_instances:
     instance = instances['Reservations'][count]['Instances'][0]
     publicip_list.append(instance['PublicIpAddress'])
-    running_time_list.append(str(datetime.now().replace(tzinfo=None)-instance['LaunchTime'].replace(tzinfo=None)))
+    running_time_list.append(str(datetime.now().replace(
+        tzinfo=None)-instance['LaunchTime'].replace(tzinfo=None)))
     for j in instance["Tags"]:
         if j["Key"] == "Name":
-            #get only the tag "Name"
+            # get only the tag "Name"
             names_list.append(j["Value"])
-    count=count+1
+    count = count + 1
 
-#starting a logger
+# starting a logger
 logger = logging.getLogger('K8S REPORTS')
 
-report={
-    'threadName':'MainThread',
+report = {
+    'threadName': 'MainThread',
     'name': 'K8S REPORTS',
     'time': datetime.now().strftime("%d/%m/%Y"),
     'running instances': num_of_instances,
